bff94e7386d123b3dd6f71724f92f94d838fd81b
waqas <waqy_arqum@hotmail.co.uk>
2022-07-20 19:54:23 +0100

    Worked on the front end of the app.py

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    name = db.Column (db.String(100))
+    bday = db.Column (db.DateTime)
+    license = db.Column (db.Integer)
+
+    def __init__(self, name, bday, license):
+        self.name = name
+        self.bday = bday
+        self.license = license
+
 
 
 @app.route('/')
 def Index():
     return render_template("index.html")
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+
+        name = request.form['Name']
+        bday = request.form['D.O.B']
+        license = request.form['License']
+
+        my_data = Data(name, bday, license)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect(url_for('Index'))
+
 
 
 if __name__ == '__main__':
